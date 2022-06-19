a389ce7b018d41d9632b69bdfd4245ddd4d495dc
Sami Karakul <samikarakul@gmail.com>
2022-06-19 14:37:42 +0300

    Converted Image component made responsive

--- components/CutieCompetition.tsx
+++ components/CutieCompetition.tsx
@@ -6,14 +6,13 @@ function CutieCompetition({image_link, changeCutie, currentCutie, endCompetition
   return (
     <div>
         <button><Image src={image_link} width="300" height="300" onClick={changeCutie} layout="responsive" alt=""/></button>
-        {currentCutie ? <button><Image src={currentCutie.image_link} width="300" height="300" onClick={endCompetition} alt=""/></button> : ""}
+        {currentCutie ? <button><Image src={currentCutie.image_link} width="300" height="300" onClick={endCompetition} layout="responsive" alt=""/></button> : ""}
 
         <style jsx>
         {`
             div{
                 display: grid;
                 grid-template-columns: 1fr 1fr;
-                // grid-gap: 10vw;
                 grid-column-gap: 10vw;
                 margin-top: 10vh;
             }
@@ -21,20 +20,26 @@ function CutieCompetition({image_link, changeCutie, currentCutie, endCompetition
                 border:none;
                 padding: 1vh 1vw;
                 box-shadow: 5px 5px 8px 5px black, -5px -5px 8px 3px black;
-                transition: box-shadow .3s ease-in-out;
+                transition: box-shadow .3s ease-in-out, transform .3s ease-in-out;
                 cursor: pointer;
+                width: 20rem;
+                height: 20rem;
             }
             div button:hover{
                 box-shadow: 10px 10px 20px 5px black, -10px -10px 20px 3px black;
+                transform: scale(1.1);
             }
 
-            @media screen and (max-width: 700px)
+            @media screen and (max-width: 800px)
             {
                 div{
                     grid-template-columns: 1fr;
                     grid-row-gap: 5vh;
                     margin-top: 5vh;
-
+                }
+                div button{
+                    width: 18rem;
+                    height: 18rem;
                 }
             }
         `}
